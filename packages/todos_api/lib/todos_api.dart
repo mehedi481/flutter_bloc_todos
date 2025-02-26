5afaeb9b0849419cd2a871a3bb21f8e0606918d2
Mehedi Hasan <mehedihasanmax95@gmail.com>
2025-02-26 15:39:55 +0600

    todos api repository and model added

--- packages/todos_api/lib/todos_api.dart
+++ packages/todos_api/lib/todos_api.dart
@@ -1,5 +1,20 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:todos_api/models/todo.dart';
+
+
+abstract class TodosApi {
+  const TodosApi();
+
+  Stream<List<Todo>> getTodos();
+
+  Future<void> saveTodo(Todo todo);
+
+  Future<void> deleteTodo(String id);
+
+  Future<int> clearCompleted();
+
+  Future<int> completeAll({required bool isCompleted});
+
+  Future<void> close();
 }
+
+class TodoNotFoundException implements Exception {}
